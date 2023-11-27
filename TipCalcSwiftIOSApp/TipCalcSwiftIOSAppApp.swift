93b9f78d48d5846daac17cb7d41c7e5c37592c66
Chacs <chackop@gmail.com>
2023-11-27 09:53:28 +0000

    Initial Commit

---
+++ TipCalcSwiftIOSApp/TipCalcSwiftIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalcSwiftIOSAppApp.swift
+//  TipCalcSwiftIOSApp
+//
+//  Created by Chacko Panikulam on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TipCalcSwiftIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
